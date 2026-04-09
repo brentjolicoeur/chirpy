83624814b99ad8e6eba07191e5023b6733da3280
brentjolicoeur <brentjolicoeur@gmail.com>
2026-04-09 13:32:06 -0400

    file server created (L1.5 complete)

--- main.go
+++ main.go
@@ -6,12 +6,18 @@ import (
 )
 
 func main() {
+	const filepathRoot = "."
+	const port = "8080"
+
 	mux := http.NewServeMux()
+	fSrv := http.FileServer(http.Dir(filepathRoot))
+	mux.Handle("/", fSrv)
 
 	srv := http.Server{
-		Addr:    ":8080",
+		Addr:    port,
 		Handler: mux,
 	}
-	log.Println("Serving on port 8080.")
+
+	log.Printf("Serving files from %s on port: %s.\n", filepathRoot, port)
 	log.Fatal(srv.ListenAndServe())
 }
